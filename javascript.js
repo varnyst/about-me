72eba806dffacf791658b8f8bab25dbb90b6cab9
Fahmi <varnyst@gmail.com>
2024-06-17 00:30:34 +0700

    - added css and javascript - added darkmode

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function displayNotif() {
+    window.alert("Contact saved, thanks!");
+}
+
+function onDark() {
+    document.getElementById("card").classList.toggle("dark");
+    document.getElementById("theme-icon").classList.toggle("fa-moon");
+}
